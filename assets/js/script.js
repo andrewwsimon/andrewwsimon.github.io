a974b6d378b53974cf0662c9eb8b35333549bf63
Andrew Simon <asimon1.61803@gmail.com>
2016-10-09 19:52:00 +0000

    v1.0

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,13 +25,10 @@ $("a").on('click', function(event) {
       // Store hash
       var hash = this.hash;
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
       }, 700, function(){
    
-        // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
       });
     } // End if
